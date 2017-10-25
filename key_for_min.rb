375b66886ecf081ad2f61e0358e1db7fcb934648
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-25 19:27:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_key = nil
    name_hash.each{|k,v|
-    if !min_key || name_hash[k] > name_hash[min_key]
+    if !min_key || name_hash[k] < name_hash[min_key]
       min_key = k
     end
   }
